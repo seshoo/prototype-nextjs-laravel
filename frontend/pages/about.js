ff15a08344393341957172d35e1a2ea7e6d61739
Sergey Sh <seshoot@gmail.com>
2021-10-09 23:33:03 +0700

    port fix

--- frontend/pages/about.js
+++ frontend/pages/about.js
@@ -34,7 +34,7 @@ export default function About(props) {
 }
 
 export async function getServerSideProps() {
-	const { data } = await axios.get(`http://proxy/api`);
+	const { data } = await axios.get(`http://backend/api`);
 
 	// Pass data to the page via props
 	return { props: { data } };
