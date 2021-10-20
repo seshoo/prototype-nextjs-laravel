5ef63677b3895ddf27e1dc8b3e9012ead27c09fd
pavel-mazhuga <pavelmazhuga@yandex.ru>
2021-10-20 23:31:42 +0700

    .

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
@@ -19,6 +20,11 @@ export default function Home() {
         <p className={styles.description}>
           Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
+        </p>
+        <p className={styles.description}>
+          <Link href="/about">
+            <a>About page</a>
+          </Link>
         </p>
 
         <div className={styles.grid}>
