08d0a28b77e4f3478265a4a1f8bcdc1af87585db
Sergey Sh <seshoot@gmail.com>
2022-01-29 21:39:58 +0700

    laravel update

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -16,9 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/api', function () {
-    return [
-        'test' => 'yes'
-    ];
-});
